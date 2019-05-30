ce97c4e5f4c2f2709fb3ed405d64b98538652b89
Hamed Waiz <hamedw96@mgmail.com>
2019-05-29 20:33:25 -0700

    Fix Toys Table

--- db/migrate/20190530015740_create_toys.rb
+++ db/migrate/20190530015740_create_toys.rb
@@ -3,8 +3,8 @@ class CreateToys < ActiveRecord::Migration[5.1]
     create_table :toys do |t|
       t.string :name, null: false
       t.references :toyable, polymorphic: true, index: true
+      t.timestamps
     end
-    add_index :toys, :name, unique: true
-    add_index :toys, [:toyable_id]
+    add_index :toys, [:name, :toyable_id, :toyable_type], unique: true
   end
 end
--- db/migrate/20190530032401_add_indices_to_toys.rb
+++
@@ -1,5 +0,0 @@
-class AddIndicesToToys < ActiveRecord::Migration[5.1]
-  def change
-    add_index :toys, [:toyable_id, :toyable_type], unique: true
-  end
-end
